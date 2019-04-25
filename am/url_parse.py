9ea28d3af09815dde767599afc7818c29e75caa7
Administrator <222@qq.com>
2019-04-25 12:06:37 +0800

    完成了爬虫解析器

--- am/url_parse.py
+++ am/url_parse.py
@@ -1,47 +1,54 @@
 from bs4 import BeautifulSoup
+from urllib.parse import urljoin
 
-html_doc = """
-<html><head><title>The Dormouse's story</title></head>
-<body>
-<p class="title"><b>The Dormouse's story</b></p>
 
-<p class="story">Once upon a time there were three little sisters; and their names were
-<a href="http://example.com/elsie" class="sister" id="link1">Elsie</a>,
-<a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> and
-<a href="http://example.com/tillie" class="sister" id="link3">Tillie</a>;
-and they lived at the bottom of a well.</p>
+class UrlParse(object):
 
-<p class="story">...</p>
-"""
-# features=html.parser 配置解析器
-soup = BeautifulSoup(html_doc, 'html.parser')
-# 直接获取某个HTML标题
-print(soup.title)
-# <title>The Dormouse's story</title>
-# 获取标签名称
-print(soup.title.name)
-# u'title'
-# 获取标签里面的值
-print(soup.title.string)
-# u'The Dormouse's story'
-# 获取唯一的父标签的名称
-print(soup.title.parent.name)
-# u'head'
-# 只能获取第一个p标签
-print(soup.p)
-# <p class="title"><b>The Dormouse's story</b></p>
-# [] 可以获取标签的属性 <a href="">  <img src="" />
-print(soup.p['class'])
-# u'title'
+    def __init__(self):
+        self.new_urls = set()
+        self.img_src = set()
 
-soup.a
-# <a class="sister" href="http://example.com/elsie" id="link1">Elsie</a>
-# find_all 获取所有的a标签
-print(soup.find_all('a'))
-# [<a class="sister" href="http://example.com/elsie" id="link1">Elsie</a>,
-#  <a class="sister" href="http://example.com/lacie" id="link2">Lacie</a>,
-#  <a class="sister" href="http://example.com/tillie" id="link3">Tillie</a>]
-if __name__ == "__main__":
-    l = soup.find_all('a')
-    for a in l:
-        print(a['href'])
+    # 创建一个方法获取HTML的地址
+    def __get_html(self, base: str, soup: BeautifulSoup):
+        arr = soup.find_all('a')
+        for a in arr:
+            href = a['href']
+            self.new_urls.add(urljoin(base, href))
+        return self.new_urls
+
+    # 创建一个方法获取HTML的图片信息
+    def __get_img(self, base: str, soup: BeautifulSoup):
+        arr = soup.find_all('img')
+        for img in arr:
+            src = img['src']
+            self.img_src.add(urljoin(base, src))
+        return self.img_src
+
+    # 编写一个方法解析传入的html代码
+    def parse_html(self, base: str, html_doc: str):
+        soup = BeautifulSoup(html_doc, 'html.parser')
+        new_urls = self.__get_html(base, soup)
+        img_src = self.__get_img(base, soup)
+        return (new_urls, img_src)
+
+
+if __name__ == '__main__':
+    html_doc = """
+    <html><head><title>The Dormouse's story</title></head>
+    <body>
+    <p class="title"><b>The Dormouse's story</b></p>
+
+    <p class="story">Once upon a time there were three little sisters; and their names were
+    <a href="http://example.com/elsie" class="sister" id="link1">Elsie</a>,
+    <a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> and
+    <a href="/tillie" class="sister" id="link3">Tillie</a>;
+    <img src='abc.png' />
+    and they lived at the bottom of a well.</p>
+
+    <p class="story">...</p>
+    """
+
+    up = UrlParse()
+    new_urls, img_src = up.parse_html("http://www.163.com", html_doc)
+    print(new_urls)
+    print(img_src)
